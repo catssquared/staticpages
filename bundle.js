a2fba8a3575ee11bb9ac58009a412350bc42767a
Vera <vera@Veras-MacBook-Air.local>
2023-11-29 23:13:00 -0800

    injecting JS into the index file hho ho ho ho

---
+++ bundle.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Hello world!"); 
+  document.getElementById('message').innerHTML = "Hello world!";
+});
+
+//console.log("Hello world!");
